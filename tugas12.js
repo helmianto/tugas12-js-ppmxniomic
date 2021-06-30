00130dddf9f8ec49d4058978c6b40a4fd190393a
Helmianto <helmianto15@gmail.com>
2021-06-30 16:30:21 +0700

    initial commit

---
+++ tugas12.js
@@ -0,0 +1,8 @@
+const buah = ['Pisang', 'Jeruk', 'Apel', 'Mangga'];
+console.log('Menampilkan array', buah);
+
+buah.pop();
+console.log('Menampilkan array dengan fungsi pop', buah);
+
+buah.shift();
+console.log('Menampilkan array dengan fungsi shift', buah);
